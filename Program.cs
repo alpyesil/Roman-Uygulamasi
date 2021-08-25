cb52c110ff282be669bbe9ae920f8b0dc9702ce5
alpysI <alperensyesil@gmail.com>
2021-08-25 14:50:07 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Roman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayfa = 10;
+            int okunansayfa = 10;
+            int gun = 0;
+
+            while (okunansayfa < 1000)
+            {
+                okunansayfa = okunansayfa + sayfa;
+                sayfa = sayfa + 5;
+                gun++;
+            }
+
+            Console.WriteLine("Kitap" + " " + gun + " " + "günde bitecek");
+        }
+    }
+}
